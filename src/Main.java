95ab7004fbc93d80b0ab4283d890dfb3fe3a6b58
Abhijeet Singh <abhijeetsingh03022003@gmail.com>
2025-07-31 01:59:55 +0530

    Adding function removeElement

--- src/Main.java
+++ src/Main.java
@@ -39,6 +39,17 @@ class Solution {
 
     return i==0;
   }
+
+  public int removeElement(int[] nums, int val) {
+    int k = 0;
+    for(int i =0; i<nums.length; i++) {
+      if (nums[i] != val){
+        nums[k] = nums[i];
+        k++;
+      }
+    }
+    return k;
+  }
 }
 
 
