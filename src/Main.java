5303c3b40bbbb01677d94ebdf90bea852743092d
Abhijeet Singh <abhijeetsingh03022003@gmail.com>
2025-08-03 19:03:01 +0530

    learnt --packages

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,9 @@
-
-class Solution {
-  public boolean isValid(String s) {
-    int n = s.length();
-
-    char[] arr = new char[n];
-    int i = 0;
-
-    for (char c : s.toCharArray()){
-      switch (c) {
-        case '(':
-          arr[i] = c;
-          i++;
-          break;
-        case '{':
-          arr[i] = c;
-          i++;
-          break;
-        case '[' :
-          arr[i] = c;
-          i++;
-          break;
-        case ')' :
-          if(i==0) return false;
-          else if (arr[i-1] != '(') return false;
-          i--;
-          break;
-        case '}':
-          if(i==0) return false;
-          else if (arr[i-1] != '{') return false;
-          i--;
-          break;
-        case ']':
-          if(i==0) return false;
-          else if (arr[i-1] != '[') return false;
-          i--;
-          break;
-      }
-    }
-
-    return i==0;
-  }
-
-  public int removeElement(int[] nums, int val) {
-    int k = 0;
-    for(int i =0; i<nums.length; i++) {
-      if (nums[i] != val){
-        nums[k] = nums[i];
-        k++;
-      }
-    }
-    return k;
-  }
-}
-
+import MyPackage.*;
 
 public class Main {
     public static void main(String[] args) {
 
-
-      ReadFile rd = new ReadFile();
-      rd.start();
+      CollectionDemo.start();
 
     }
 }
